03f1fcc4271abd72ff9dcbec76071a631fcffc91
Blake Preston <blake.p.preston@gmail.com>
2022-06-03 12:57:18 -0700

    Fixing netlify issue

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import Amplify from 'aws-amplify'
-import awsconfig from './aws-exports'
+import awsconfig from './aws-exports.js'
 import {applyPolyfills, defineCustomElements} from '@aws-amplify/ui-components/loader'
 
 applyPolyfills().then(()=>{
